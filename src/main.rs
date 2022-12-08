688353948d154e718979338c0dd1a033cceacac2
MFaiqKhan <faiqkhanthinkcentre@yahoo.com>
2022-12-08 11:50:30 +0500

    added modules and crates

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 // The connection between a program and a data source or destination is called a stream. An input stream handles data flowing into a program. An output stream handles data flowing out of a program.
 #![allow(unused)] // Allow unused code
 
+mod variable; // import the variable module from the variable.rs file
+mod datatypes; // import the datatypes module from the datatypes.rs file
+mod maths;
+mod random;
+mod if_statement;
+mod ternary_operator;
+
+pub use crate::variable::variable; // This is will run the variable function from the variable module
+
+use crate::datatypes::datatypes; // This is will run the datatypes function from the datatypes module
+use crate::maths::maths; // This is will run the maths function from the maths module
+use crate::random::random;
+use crate::if_statement::iff;
+use crate::ternary_operator::ternary_operator;
+
+
 use std::io;  // Import the io library from the standard library
 use rand::Rng;  // what this library does is generate random numbers for us
 use std::io::{Write, BufReader, BufRead, ErrorKind}; // bufreader is a buffered reader that reads from a file
@@ -16,6 +32,33 @@ fn main() {
     println!("Hello, world!"); // println! is a macro that prints to the console , everything ending with a ! is a macro
 
     println!("Hello {}! {}", name.trim_end(), greeting); // trim_end() removes the trailing newline character from the string, and {} acting as a space holder for the arguments passed after the string literal in the println! macro call 
+
+    println!("-------------------------------Variable.rs----------------------------------");
+    // variable.rs
+    variable::variable(); // calling the variable function from the variable module
+
+    // datatypes
+    println!("-------------------------------datatypes.rs----------------------------------");
+    datatypes::datatypes(); // calling the datatypes function from the datatypes module
+
+    // maths 
+    println!("-------------------------------maths.rs----------------------------------");
+    maths::maths();
+
+    // random
+    println!("-------------------------------random.rs----------------------------------");
+    random::random();
+
+    // if_statement
+    println!("-------------------------------if_statement.rs----------------------------------");
+    if_statement::iff();
+
+    // ternary_operator
+    println!("-------------------------------ternary_operator.rs----------------------------------");
+    ternary_operator::ternary_operator();
+    
+
+
 }
 
 
@@ -36,3 +79,6 @@ fn main() {
 //rather than being provided by a library like in some other languages. 
 //This means that the Rust compiler can statically check for error handling and 
 //will not allow the program to compile if it detects any issues with the error handling in the code.
+
+// difference b/w pub use crate::variable::variable vs use crate::datatypes::datatypes is that pub use crate::variable::variable is a public function and use crate::datatypes::datatypes is a private function
+// and we can call the public function from any other module but we can't call the private function from any other module
